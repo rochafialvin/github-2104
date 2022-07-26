1ec41c012c700ac5d07200a9420eb9f3b59f008c
rochafialvin <rochafi.dev@gmail.com>
2022-07-26 09:42:20 +0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome home !");
+  console.log("Welcome home !");
+});
+
+app.listen(port, (error) => {
+  if (error) return console.error("Api failed to start", error);
+  console.log(`API is running at ${port}`);
+});
